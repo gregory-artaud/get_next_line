17b5ad5b569e630969c9a7559a8fcd1286f4bee3
ARTAUD Grégory <gregory.rtd@gmail.com>
2020-07-18 21:41:17 +0200

    junk code in get_next_line.c, base of the project

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gregory <gregory@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/18 19:34:22 by gregory           #+#    #+#             */
+/*   Updated: 2020/07/18 21:36:13 by gregory          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFFER_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
